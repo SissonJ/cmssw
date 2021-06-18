8b45f48bc80c234d7a4b04880888aabcb32b7cb8
Jack Sisson <sissonjack67@gmail.com>
2021-06-18 10:36:01 -0500

    Changed name to 'EfficiencyExtras' and added a instantaneous ls efficiency plot

--- DQM/SiPixelPhase1Track/python/SiPixelPhase1Extras_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
-
-
-SiPixelPhase1Extras = DQMEDHarvester("SiPixelPhase1Extras",
-                                     EffFolderName = cms.string('PixelPhase1/Tracks/'),
-                                     VtxFolderName = cms.string('Tracking/TrackParameters/generalTracks/GeneralProperties/'),
-)
